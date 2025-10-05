cbd680f4bbd41a20eec833165c6025a7cc95f52e
Bersen Pajaziti <bersenp@hotmail.com>
2025-10-05 15:47:57 +0200

    add description

--- packages/taskwish/example/usecases/simple.ts
+++ packages/taskwish/example/usecases/simple.ts
@@ -1,8 +1,11 @@
 import { run, UseCase } from "../../src";
 
-export default UseCase("Simple").steps(
-  run("Slack.sendMessage", {
-    channel: "#general",
-    text: "Hi",
-  })
-);
+export default UseCase("Simple")
+  .description("Send message to slack")
+
+  .steps(
+    run("Slack.sendMessage", {
+      channel: "#general",
+      text: "Hi",
+    })
+  );
