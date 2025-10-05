3aff8af09cc5723d7adbf0085f4d6bb66851bd88
Bersen Pajaziti <bersenp@hotmail.com>
2025-10-05 22:34:03 +0200

    nested input

--- packages/taskwish/example/usecases/loop.ts
+++ packages/taskwish/example/usecases/loop.ts
@@ -1,14 +1,14 @@
 import { Loop, End, Range, UseCase } from "../../src";
 
 export default UseCase("Say hello")
-  .on({ language: "string" })
+  .on({ user: { name: "string", age: "number" } })
 
   .steps(
     Loop(Range(0, 10)),
 
-    ["asdasd", ({ input }) => input.language],
+    ["asdasd", ({ input }) => input.user.name],
 
     End(Loop),
 
-    scope => scope.asdasd
+    (scope) => scope.asdasd
   );
