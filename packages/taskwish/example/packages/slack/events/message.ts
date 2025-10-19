ac7d346ebe2b84cf2c87967dcf62af45d7a9c6c4
Bersen Pajaziti <bersenp@hotmail.com>
2025-10-19 15:24:37 +0200

    message types

--- packages/taskwish/example/packages/slack/events/message.ts
+++ packages/taskwish/example/packages/slack/events/message.ts
@@ -1,14 +1,33 @@
+import { type } from "arktype";
 import { Event } from "../../../../src";
 
-export default Event("message")
-  .data({
+export const message = type({
+  type: "'message'",
+  channel: "string",
+  user: "string",
+  text: "string",
+  ts: "string",
+})
+  .or({
     type: "'message'",
-    subtype: "'bot_message'|'bot_2_message'",
+    subtype: "'bot_message'",
+    ts: "string",
+    text: "string",
+    bot_id: "string",
+    username: "string",
+    icons: "object",
+  })
+  .or({
+    type: "'message'",
+    subtype: "'me_message'",
     channel: "string",
     user: "string",
     text: "string",
     ts: "string",
-  })
+  });
+
+export default Event("message")
+  .data(message)
 
   .describe({
     description: "A message was sent to a channel",
--- packages/taskwish/example/usecases/slack.ts
+++ packages/taskwish/example/usecases/slack.ts
@@ -1,7 +1,12 @@
+import { match } from "ts-pattern";
 import { UseCase } from "../../src";
 import slack from "../packages/slack";
 
 export default UseCase("Slack")
   .trigger(slack.events.message)
 
-  .steps(["fist step", ($) => $.event.data]);
+  .steps(($) =>
+    match($.input)
+      .with({ subtype: "bot_message" }, (message) => message.bot_id)
+      .with({ subtype: "me_message" }, (message) => message.user)
+  );
