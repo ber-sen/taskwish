d06f6093ee2ea25987885f329850ff15a8e07ad2
Bersen Pajaziti <bersenp@hotmail.com>
2025-10-24 18:44:42 +0200

    add configuration

--- packages/taskwish/example/usecases/agent.ts
+++ packages/taskwish/example/usecases/agent.ts
@@ -4,10 +4,11 @@ export default UseCase("Chat bot")
   .trigger({ messages: { name: "string", age: "number" } })
 
   .steps(($) =>
-    Agent("lorem", {
-      system: "You are a helpful assistant.",
-      model: "openai/gpt-4o",
-    })
+    Agent("lorem")
+      .configuration({
+        system: "You are a helpful assistant.",
+        model: "openai/gpt-4o",
+      })
       .abilities()
       .on(["user reject"])
 
--- packages/taskwish/src/agent/agent.ts
+++ packages/taskwish/src/agent/agent.ts
@@ -1,10 +1,10 @@
 export interface AgentFactory<Params, Scope extends Record<any, any> = {}> {
+  configuration(...skills: any): AgentFactory<Params, Scope>;
   abilities(...skills: any): any;
 }
 
 export const Agent = <const Params extends string>(
-  name: Params,
-  options: any
+  name: Params
 ): AgentFactory<Params> => {
   return name as any;
 };
