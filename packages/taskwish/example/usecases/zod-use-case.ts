2c0a3bd4847908e5718f5bc522b8c7727e43dce0
Bersen Pajaziti <bersenp@hotmail.com>
2025-11-02 00:50:50 +0100

    object variant

--- packages/taskwish/example/usecases/zod-use-case.ts
+++ packages/taskwish/example/usecases/zod-use-case.ts
@@ -11,14 +11,30 @@ export default UseCase("Say hello")
   })
 
   .steps(
-    ["asdasd", ({ input }) => input],
-
-    [
-      "lorem",
-      ({ action, input }) =>
+    { step1: ({ input }) => input },
+    {
+      step2: ({ input, action }) =>
         action.slack.sendMessage({
           channel: "#general",
           text: `Does someone speak ${input.language}?`,
         }),
-    ],
+    },
   );
+
+
+type UnionToIntersection<U> =
+  (U extends any ? (x: U) => void : never) extends (x: infer I) => void ? I : never;
+
+// Turn a union into an overloaded function, then extract its parameter tuple
+type UnionToTuple<U> = 
+  UnionToIntersection<U extends any ? (x: U) => void : never> extends (x: infer I) => void
+    ? [...UnionToTuple<Exclude<U, I>>, I]
+    : [];
+
+// Count the number of keys
+type KeyCount<T> = UnionToTuple<keyof T>['length'];
+
+type Zero = KeyCount<{}>;               // 0
+type One = KeyCount<{ a: 1 }>;          // 1
+type Two = KeyCount<{ a: 1; b: 2 }>;    // 2
+
