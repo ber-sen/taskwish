eec34dc01cc29be780c3eea0c766a0d44a963c6d
Bersen Pajaziti <bersenp@hotmail.com>
2025-11-20 23:59:50 +0100

    add actorId to thread

--- packages/sica/src/types.ts
+++ packages/sica/src/types.ts
@@ -123,6 +123,7 @@ export namespace Sica {
 
   export interface Thread {
     id: ThreadId;
+    actorId?: UUIDv5String
     state: "new" | "active" | "waiting" | "finalized" | "renewed";
     messages: SicaMessage.AnyMessage[];
   }
