201ab4513c7681dc804ba265484ee94641587e4e
Bersen Pajaziti <bersenp@hotmail.com>
2025-11-24 18:11:29 +0100

    fix

--- packages/sica/example/usecases/pipe step.ts
+++ packages/sica/example/usecases/pipe step.ts
@@ -7,24 +7,28 @@ export default UseCase("Sub steps")
 
   .steps(
     ({ input }) =>
-      Step("asdads")
-        .run(async function* () {
+      Step("asdads").run(
+        async function* () {
           yield Message.User([
             { type: "text", text: "asd" },
             { type: "text", text: "asdasd" },
           ]);
           yield 2;
           yield 3;
-        })
-        .pipe(Source.pipeTo(Response)),
+        },
+        Source.pipeTo(Response),
+        Source.pipeTo(Response),
+        Source.pipeTo(Response),
+        Source.pipeTo(Response),
+        Source.pipeTo(Response)
+      ),
 
     ({ action, input }) =>
-      Step("asdads")
-        .run(
-          action.slack.sendMessage({
-            channel: "#general",
-            text: `Does someone speak ${input.language}?`,
-          })
-        )
-        .pipe(Source.pipeTo(Response))
+      Step("asdads").run(
+        action.slack.sendMessage({
+          channel: "#general",
+          text: `Does someone speak ${input.language}?`,
+        }),
+        Source.pipeTo(Response)
+      )
   );
