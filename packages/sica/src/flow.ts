7bdb45c6ba7d2d263b0a37c84fd9e0abff4f11af
Bersen Pajaziti <bersenp@hotmail.com>
2025-11-29 21:35:43 +0100

    flow

--- packages/sica/src/flow.ts
+++ packages/sica/src/flow.ts
@@ -1,17 +1,11 @@
 import { Sica } from "./types";
 
-export function Flow<
-  Type extends string[],
-  Scope = {},
-  Group extends null | string[] = null,
->(options: {
-  type: Type;
-  group: Group;
-  scope: Scope;
-}): Sica.Flow<Type, Scope, Group> {
+export function Flow<Type extends string>(type: Type) {
   return {
-    [Sica.Type]: options.type,
-    group: options.group,
-    scope: options.scope,
+    scope: <Scope>(scope: Scope) => ({
+      [Sica.Type]: [type],
+      group: null,
+      scope,
+    }),
   };
 }
--- packages/sica/src/steps/if-else.ts
+++ packages/sica/src/steps/if-else.ts
@@ -1,25 +1,11 @@
 import { Flow } from "../flow";
-import { Sica } from "../types";
 
 export type Truthy<T> = T extends false | "" | 0 | null | undefined ? never : T;
 
 export const If = <Condition>(condition: Condition) =>
-  Flow({
-    type: ["if"],
-    scope: { condition: condition as Truthy<Condition> },
-    group: null,
-  });
+  Flow("if").scope({ condition: condition as Truthy<Condition> });
 
-export const ElseIf = <const Condition>(
-  condition: Condition
-): Sica.Flow<["else-if"], { condition: Truthy<Condition> }> => ({
-  [Sica.Type]: ["else-if"],
-  group: null,
-  scope: { condition } as never,
-});
+export const ElseIf = <const Condition>(condition: Condition) =>
+  Flow("else-if").scope({ condition: condition as Truthy<Condition> });
 
-export const Else = (): Sica.Flow<["else"], { condition: false }> => ({
-  [Sica.Type]: ["else"],
-  group: null,
-  scope: {} as never,
-});
+export const Else = () => Flow("else").scope({ condition: false });
