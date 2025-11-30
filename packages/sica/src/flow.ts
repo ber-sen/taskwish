405fd21340e07ab85e1a91e4304eec08ea16b710
Bersen Pajaziti <bersenp@hotmail.com>
2025-11-30 10:56:59 +0100

    fix flow

--- packages/sica/src/flow.ts
+++ packages/sica/src/flow.ts
@@ -2,7 +2,7 @@ import { Sica } from "./types";
 
 export function Flow<Type extends string>(type: Type) {
   return {
-    params: <Params>(params: Params) => ({
+    params: <Params>(params: Params): Sica.Flow<[Type], Params, null> => ({
       [Sica.Type]: [type],
       group: null,
       params,
