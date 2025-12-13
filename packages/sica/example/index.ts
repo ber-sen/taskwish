22964bb9c1e4164eb15026e5337b953bf6346501
Bersen Pajaziti <bersenp@hotmail.com>
2025-12-13 12:24:07 +0100

    app

--- packages/sica/example/index.ts
+++ packages/sica/example/index.ts
@@ -1,1 +1,11 @@
-export { default } from "./package"
+export default App(
+  "My app",
+  import("./package"),
+
+  ["GET:/api/say-hello/:language", "example:say-hello"],
+  ["CMD:say-hello :language", "example:say-hello"],
+  ["GMAIL:pajaziti.bersen@gmail.com", "example:io"],
+  ["SLACK:pajaziti.bersen@gmail.com", "example:io"],
+
+  Provide("env", process.env)
+);
--- packages/sica/example/package.ts
+++ packages/sica/example/package.ts
@@ -4,14 +4,8 @@ export default Package(
   "Example",
 
   import("./actions"),
-  import("./agents"),
   import("./packages/slack"),
-
-  ["POST:/api/simple", import("./usecases/simple")],
-  ["GET:/api/say-hello/:language", import("./usecases/say-hello")],
-  ["CMD:say-hello :language", import("./usecases/say-hello")],
-  ["GMAIL:pajaziti.bersen@gmail.com", import("./usecases/io")],
-  ["SLACK:pajaziti.bersen@gmail.com", import("./usecases/io")],
-
-  Provide("env", process.env)
+  import("./usecases/simple"),
+  import("./usecases/say-hello"),
+  import("./usecases/io")
 );
