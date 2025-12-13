ba1b767cf26f8bad55a83f07714c872003d16982
Bersen Pajaziti <bersenp@hotmail.com>
2025-12-13 15:45:37 +0100

    app

--- packages/sica/example/index.ts
+++ packages/sica/example/index.ts
@@ -2,13 +2,10 @@ export default App(
   "My app",
   import("./package"),
 
-  {
-    "GET:/api/say-hello/:language": ({ example }) => example.helloWold,
-    "CMD:say-hello :language": (app) => app.example.helloWold,
-    "GMAIL:pajaziti.bersen@gmail.com": (app) => app.example.io,
-    "SLACK:pajaziti.bersen@gmail.com": (app) =>
-      app.example.io({ from: "lorem@ipsum" }),
-  },
+  ["GET", "/api/say-hello/:language", "example::helloWold"],
+  ["CMD", "say-hello :language", "example::helloWold"],
+  ["GMAIL", "pajaziti.bersen@gmail.com", "example::io"],
+  ["SLACK", "pajaziti.bersen@gmail.com", "example::io"],
 
   Provide("env", process.env)
 );
