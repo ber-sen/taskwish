5043c5186b38b1f9cf94af70b7b09e02209181a0
Bersen Pajaziti <bersenp@hotmail.com>
2026-01-10 23:25:01 +0100

    cleanup

--- packages/sica/src/steps/steps.ts
+++ packages/sica/src/steps/steps.ts
@@ -1,122 +1,30 @@
-import { Boria } from "../boria";
 import { PrettyScope } from "../helpers";
-import { Sica } from "../types";
-
-type Return = Sica.NullaryAction<
-  () => {
-    success: boolean;
-  }
->;
 
 const Last = Symbol.for("Last");
 
-type AnyStep<Scope> =
-  | { name: string; run: (scope: PrettyScope<Scope>) => any }
-  | {
-      name: string;
-      type: string[];
-      middleware: (scope: PrettyScope<Scope>, next: any) => any;
-    }
-  | ((
-      scope: PrettyScope<Scope>
-    ) => any | Sica.Flow<any, any, null> | Boria.Message<any, any>)
-  | Sica.Flow<any, any, null>
-  | Boria.Message<any, any>;
-
-type InferStepRecord<Step> = Step extends {
-  name: infer Name;
-  run: infer Fn;
-}
-  ? Name extends string
-    ? Fn extends (args: any) => any
-      ? Record<Name, ReturnType<Fn>>
-      : {}
-    : {}
-  : Step extends Sica.Flow<any, infer Scope, null>
-    ? Scope
-    : {};
-
-export interface OldSteps<Scope extends Record<any, any>> {
-  <const S0 extends AnyStep<Scope>, S0R>(
-    ...trumpets: [step: S0 & { run: S0R }]
-  ): Return;
-  <
-    const S0 extends AnyStep<Scope>,
-    const S1 extends AnyStep<Scope & InferStepRecord<S0>>,
-  >(
-    ...trumpets: [step: S0, step: S1]
-  ): Return;
-  <
-    const S0 extends AnyStep<Scope>,
-    const S1 extends AnyStep<Scope & InferStepRecord<S0>>,
-    const S2 extends AnyStep<Scope & InferStepRecord<S0> & InferStepRecord<S1>>,
-  >(
-    ...trumpets: [step: S0, step: S1, step: S2]
-  ): Return;
-  <
-    const S0 extends AnyStep<Scope>,
-    const S1 extends AnyStep<Scope & InferStepRecord<S0>>,
-    const S2 extends AnyStep<Scope & InferStepRecord<S0> & InferStepRecord<S1>>,
-    const S3 extends AnyStep<
-      Scope & InferStepRecord<S0> & InferStepRecord<S1> & InferStepRecord<S2>
-    >,
-  >(
-    ...trumpets: [step: S0, step: S1, step: S2, step: S3]
-  ): Return;
-  <
-    const S0 extends AnyStep<Scope>,
-    const S1 extends AnyStep<Scope & InferStepRecord<S0>>,
-    const S2 extends AnyStep<Scope & InferStepRecord<S1>>,
-    const S3 extends AnyStep<
-      Scope & InferStepRecord<S0> & InferStepRecord<S1> & InferStepRecord<S2>
-    >,
-    const S4 extends AnyStep<
-      Scope &
-        InferStepRecord<S0> &
-        InferStepRecord<S1> &
-        InferStepRecord<S2> &
-        InferStepRecord<S3>
-    >,
-  >(
-    ...trumpets: [step: S0, step: S1, step: S2, step: S3, step: S4]
-  ): Return;
-}
-
-// export const Parallel = (
-//   name?: string
-// ): Sica.Flow<["parallel"], null, any> => ({
-//   [Sica.Type]: ["parallel"],
-//   group: null,
-// });
-
-// export const Return = (): Sica.Flow<["return"], null, any> => ({
-//   [Sica.Type]: ["return"],
-//   group: null,
-// });
-
-export interface ScopePipe {
-  scope?: (input: any) => any;
-}
+export type StepsReturn<Scope> = typeof Last extends keyof Scope
+  ? Scope[typeof Last]
+  : Scope;
 
 export interface Steps<Scope extends Record<any, any>> {
   <A>(
-    step: { scope: (input: Scope) => A } | ((this: Scope) => A)
-  ): typeof Last extends keyof A ? A[typeof Last] : A;
+    step: { step: (input: Scope) => A } | ((this: Scope) => A)
+  ): StepsReturn<A>;
   <A, B>(
-    step1: { scope: (input: Scope) => A },
-    step2: { scope: (input: A) => B }
-  ): typeof Last extends keyof B ? B[typeof Last] : B;
+    step1: { step: (input: Scope) => A },
+    step2: { step: (input: A) => B }
+  ): StepsReturn<B>;
   <A, B, C>(
-    step1: { scope: (input: Scope) => A },
-    step2: { scope: (input: A) => B },
-    step3: { scope: (input: B) => C }
-  ): C;
+    step1: { step: (input: Scope) => A },
+    step2: { step: (input: A) => B },
+    step3: { step: (input: B) => C }
+  ): StepsReturn<C>;
   <A, B, C, D>(
-    step1: { scope: (input: Scope) => A },
-    step2: { scope: (input: A) => B },
-    step3: { scope: (input: B) => C },
-    step4: { scope: (input: C) => D }
-  ): D;
+    step1: { step: (input: Scope) => A },
+    step2: { step: (input: A) => B },
+    step3: { step: (input: B) => C },
+    step4: { step: (input: C) => D }
+  ): StepsReturn<D>;
 }
 
 export function Step<
@@ -130,7 +38,7 @@ export function Step<
     | [(this: PrettyScope<Scope>) => Result]
     | [(this: PrettyScope<Scope>) => Result, { retry: number }]
 ): {
-  scope: (
+  step: (
     scope: Scope
   ) => Name extends string
     ? Record<Name, Result> &
