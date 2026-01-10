4084124272fc88c82a80f5a750c127f8cd50f162
Bersen Pajaziti <bersenp@hotmail.com>
2026-01-10 14:33:55 +0100

    test

--- packages/sica/src/steps/steps.ts
+++ packages/sica/src/steps/steps.ts
@@ -92,6 +92,10 @@ export interface OldSteps<Scope extends Record<any, any>> {
 //   group: null,
 // });
 
+export interface ScopePipe {
+  scope?: (input: any) => any;
+}
+
 export interface Steps<Scope extends Record<any, any>> {
   <A>(step: { scope: (input: Scope) => A }): A;
   <A, B>(
@@ -104,8 +108,12 @@ export interface Steps<Scope extends Record<any, any>> {
     step3: { scope: (input: B) => C }
   ): C;
   <A, B, C, D>(
-    step1: { scope: (input: Scope) => A },
-    step2: { scope: (input: A) => B },
+    step1:
+      | { scope: (input: Scope) => A }
+      | (A extends { if: boolean } ? A : never),
+    step2: {
+      scope: (input: A extends { if: boolean } ? Scope & A : A) => B;
+    },
     step3: { scope: (input: B) => C },
     step4: { scope: (input: C) => D }
   ): D;
@@ -150,7 +158,10 @@ export const Steps: Steps<Scope> = () => {
 };
 
 const a = Steps(
-  Step("get prompt", () => "Say hello in spanish"),
+  { if: 2 > 2 },
+  // Step("init", ($) => "Say hello in spanish"),
+
+  Step("get prompt", ($) => "Say hello in spanish"),
 
   Step(
     "generate text",
