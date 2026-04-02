f099670b668d786452dba673034df0739f23ca18
Bersen Pajaziti <bersenp@hotmail.com>
2026-04-02 22:44:39 +0200

    arraybuffer

--- packages/taskwish/examples/test/main.js
+++ packages/taskwish/examples/test/main.js
@@ -1,11 +1,19 @@
 // main.js
-const { Worker } = require("node:worker_threads")
-const worker = new Worker("./worker.js")
+const { Worker } = require("node:worker_threads");
+const worker = new Worker("./worker.js");
+
+import { randomBytes } from "crypto";
+
+const random = randomBytes(16);
+const buffer = random.buffer.slice(
+  random.byteOffset,
+  random.byteOffset + random.byteLength,
+);
 
 const { port1 } = new MessageChannel();
 worker.postMessage({ port1 }, [port1]);
+worker.postMessage({ buffer }, [buffer]);
 
 worker.on("message", (message) => {
-    console.log(message)
-})
-
+  console.log(message);
+});
--- packages/taskwish/examples/test/worker.js
+++ packages/taskwish/examples/test/worker.js
@@ -1,6 +1,14 @@
-// const { parentPort } = require("worker_threads");
-// const { port1 } = new MessageChannel();
-// parentPort.postMessage({ port1 }, port1);
+const { parentPort } = require("worker_threads");
+
+import { randomBytes } from "crypto";
+
+const random = randomBytes(16);
+const buffer = random.buffer.slice(
+  random.byteOffset,
+  random.byteOffset + random.byteLength,
+);
+
+parentPort.postMessage({ buffer }, { transfer: [buffer] });
 
 globalThis.addEventListener("message", (message) => {
   console.log(message);
