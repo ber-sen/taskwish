af9a2d5b476049f06fed8a9392839f7b5ff12a2f
Bersen Pajaziti <bersenp@hotmail.com>
2026-04-04 14:09:40 +0200

    Broadcast

--- packages/taskwish/examples/test/main.js
+++ packages/taskwish/examples/test/main.js
@@ -2,18 +2,28 @@
 const { Worker } = require("node:worker_threads");
 const worker = new Worker("./worker.js");
 
-import { randomBytes } from "crypto";
+// import { randomBytes } from "crypto";
 
-const random = randomBytes(16);
-const buffer = random.buffer.slice(
-  random.byteOffset,
-  random.byteOffset + random.byteLength,
-);
+// const random = randomBytes(16);
+// const buffer = random.buffer.slice(
+//   random.byteOffset,
+//   random.byteOffset + random.byteLength,
+// );
 
-const { port1 } = new MessageChannel();
-worker.postMessage({ port1 }, [port1]);
-worker.postMessage({ buffer }, [buffer]);
+// const { port1 } = new MessageChannel();
+// worker.postMessage({ port1 }, [port1]);
+// worker.postMessage({ buffer }, [buffer]);
 
-worker.on("message", (message) => {
-  console.log(message);
-});
+// worker.on("message", (message) => {
+//   console.log(message);
+// });
+
+const bc = new BroadcastChannel("tasks");
+
+bc.onmessage = (event) => {
+  bc.postMessage("lorem")
+  console.log("channel parent")
+  console.log(event);
+};
+
+
--- packages/taskwish/examples/test/worker.js
+++ packages/taskwish/examples/test/worker.js
@@ -1,15 +1,24 @@
-const { parentPort } = require("worker_threads");
+// const { parentPort } = require("worker_threads");
 
-import { randomBytes } from "crypto";
+// import { randomBytes } from "crypto";
 
-const random = randomBytes(16);
-const buffer = random.buffer.slice(
-  random.byteOffset,
-  random.byteOffset + random.byteLength,
-);
+// const random = randomBytes(16);
+// const buffer = random.buffer.slice(
+//   random.byteOffset,
+//   random.byteOffset + random.byteLength,
+// );
 
-parentPort.postMessage({ buffer }, { transfer: [buffer] });
+// parentPort.postMessage({ buffer }, { transfer: [buffer] });
 
-globalThis.addEventListener("message", (message) => {
-  console.log(message);
-});
+// globalThis.addEventListener("message", (message) => {
+//   console.log(message);
+// });
+
+const bc = new BroadcastChannel("tasks");
+
+bc.onmessage = (event) => {
+  console.log("channel");
+  console.log(event);
+};
+
+bc.postMessage("This is a test message.");
