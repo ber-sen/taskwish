5d123ec152f0b29b8d58d4087b3b3ed001710f17
Bersen Pajaziti <bersenp@hotmail.com>
2026-04-20 19:12:48 +0200

    test

--- packages/core/test/type.ts
+++ packages/core/test/type.ts
@@ -1,3 +1,7 @@
 import { type } from "arktype";
 
 console.log(type({ name: "string", "lorem?": { name: "string" } }).toString());
+
+function asd(this: any, input: number) {
+  return this?.fetch?.(`google ${input}`) ?? `google ${input}`;
+}
