d40ef10cb5cb8e374ce6e873266d1be106d54be1
Bersen Pajaziti <bersenp@hotmail.com>
2026-04-20 21:12:01 +0200

    service

--- packages/core/example/service/slack.ts
+++ packages/core/example/service/slack.ts
@@ -1,16 +1,5 @@
 import { Service } from "../../src/service";
 
-const { Slack } = Service("Slack");
-
-// hello action
-export const { postMessage } = Slack()
-  .action("postMessage")
-
-  .input<{ channel: string }>()
-
-  .run(function () {
-    return this.connection.client.callTool({
-      name: "post_message",
-      arguments: this.input,
-    });
-  });
+export const { Slack } = Service("Slack")
+  .action<SendMessageI, SendMessageO>("sendMessage", "send_message")
+  .action<SendMessageI, SendMessageO>("sendMessage", "send_message");
