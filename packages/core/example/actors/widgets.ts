30d364232f6b2bdc3a1e095bd22c0b81d0f6c97b
Bersen Pajaziti <bersenp@hotmail.com>
2026-06-30 13:29:59 +0200

    widgets

---
+++ packages/core/example/actors/widgets.ts
@@ -0,0 +1,35 @@
+"use server";
+
+import { Actor, Step, Event } from "../../src";
+
+export const { Greeter } = Actor("Greeter").def(
+  Event("UserWelcomed", { name: "string" }),
+);
+
+export const { hello } = Greeter()
+  .on("Command", "hello")
+
+  .input({ name: "string" })
+
+  .run(
+    Card(Text, Actions([Button, Button])).as("Lorem"),
+
+    Step("greet", function () {
+      return this.reply("Lorem",{
+        title: "Order #1234",
+        Text: "Your order has been received!",
+        Actions: [
+          { id: "approve", label: "Approve", style: "primary" },
+          { id: "approve", label: "Approve", style: "primary" },
+        ],
+      })
+    ),
+  );
+
+const { Credentials } = App("Credentials", {
+  "/": Screen(List()),
+})
+
+const app = Credentials({
+	"/": { name: "List", List: { data: [1,2,3] }}
+});
