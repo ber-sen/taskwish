c50328eb43f71838050145a25b6fe7e82de0d786
Bersen Pajaziti <bersenp@hotmail.com>
2026-07-01 21:53:02 +0200

    fix

--- packages/example/src/biller.ts
+++ packages/example/src/biller.ts
@@ -1,13 +1,16 @@
 import { Actor } from "@taskwish/core";
 
 import { Greeter } from "./greeter";
+import { sleep } from "bun";
 
 export const { Biller } = Actor("Biller").use(Greeter);
 
 export const { onGreeterMessage } = Biller()
   .on("Greeter::Message")
 
-  .run(function () {
+  .run(async function () {
+    await sleep(3000)
+
     return {
       invoice: `Invoice created from greeter message: ${this.input.name}`,
     };
