6ec3961b516f7d47e3bdc0364f802f8a8a4327f2
Ber-Sen <79872416+ber-sen@users.noreply.github.com>
2026-07-02 20:46:21 +0200

    handle port (#25)

--- packages/example/src/node.ts
+++ packages/example/src/node.ts
@@ -4,7 +4,7 @@ import { startupMessage } from "./helpers";
 
 const node = await Node("example", {
   apiKey: process.env.TW_API_KEY,
-  port: Number(process.env.PORT ?? 3000),
+  port: 3000,
   workspace: [
     import("./greeter"),
     import("./biller"),
