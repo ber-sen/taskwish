6cd89769ceee42d2bd619a555ad5d36873b451b9
Ber-Sen <79872416+ber-sen@users.noreply.github.com>
2026-07-31 01:20:12 +0200

    bare (#33)

--- packages/bare-example/src/morph.ts
+++ packages/bare-example/src/morph.ts
@@ -1,11 +1,11 @@
 import { mkdir } from "node:fs/promises";
 
-import { morph } from "@taskwish/bare-metal";
+import { morph } from "@taskwish/bare";
 
 const input = new URL("./two-steps.ts", import.meta.url);
-const output = new URL("./bare-metal/two-steps.ts", import.meta.url);
+const output = new URL("./bare/two-steps.ts", import.meta.url);
 
-await mkdir(new URL("./bare-metal", import.meta.url), { recursive: true });
+await mkdir(new URL("./bare", import.meta.url), { recursive: true });
 await Bun.write(
   output,
   morph(await Bun.file(input).text(), {
