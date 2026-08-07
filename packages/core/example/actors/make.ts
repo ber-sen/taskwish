10d66d1910c49fc422229553acd6ddeff6f7142c
Bersen Pajaziti <bersenp@hotmail.com>
2026-08-07 11:27:41 +0200

    service

---
+++ packages/core/example/actors/make.ts
@@ -0,0 +1,20 @@
+const { myActor } = Actor("MyActor");
+
+export const { runSteps } = myActor()
+  .on("Command", "runSteps")
+
+  .input({ message: "string" })
+
+  .run(
+    Step("firstStep", function () {
+      return "step 1";
+    }),
+
+    Step("lastStep", function () {
+      return this.firstStep.length;
+    }),
+  );
+
+export const { MyActor } = myActor().service({
+  public: [runSteps],
+});
