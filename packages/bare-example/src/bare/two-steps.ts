364204403a247fe113d475655b84e2f8ce13221d
Bersen Pajaziti <bersenp@hotmail.com>
2026-08-10 16:48:40 +0200

    final

--- packages/bare-example/src/bare/two-steps.ts
+++ packages/bare-example/src/bare/two-steps.ts
@@ -3,27 +3,47 @@ function normalizeName(name: string) {
 }
 
 export async function greet(input: { name: string }) {
-  return run_greet({ input });
+  const generator = stream_greet({ input });
+
+  while (true) {
+    const next = await generator.next();
+
+    if (next.done) {
+      return next.value;
+    }
+  }
 }
 
-async function run_greet(params: {
+export async function run_greet(params: {
   input: { name: string };
   trace?: (data: unknown) => void;
 }) {
-  const trace =
-    params.trace ?? ((data: unknown) => console.log(JSON.stringify(data)));
+  const generator = stream_greet(params);
 
+  while (true) {
+    const next = await generator.next();
+
+    if (next.done) {
+      return next.value;
+    }
+  }
+}
+
+async function* stream_greet(params: {
+  input: { name: string };
+  trace?: (data: unknown) => void;
+}) {
   const input = params.input;
 
-  trace({ ">>": "Greeter", input: input });
+  yield { ">>": "Greeter", input: input };
 
   const salutation = Math.random() > 0.5 ? "Hello" : "HI";
 
-  trace({ ">>": "Greeter::greet", result: salutation });
+  yield { ">>": "Greeter::greet", result: salutation };
 
   const greet = `${salutation} ${normalizeName(input.name)}.`;
 
-  trace({ ">>": "Greeter", result: greet });
+  yield { ">>": "Greeter", result: greet };
 
   return greet;
 }
@@ -32,6 +52,9 @@ export const Greeter = {
   greet,
   run: {
     greet: run_greet,
+  },
+  stream: {
+    greet: stream_greet,
   },
 };
 
